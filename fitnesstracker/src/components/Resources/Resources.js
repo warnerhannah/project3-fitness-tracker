cb909630289e2e230ba4407fa3cced765e770ab2
Warnerhannah <warnerhannah1@yahoo.com>
2019-04-23 13:44:12 -0700

    original file creation and blank setups

---
+++ fitnesstracker/src/components/Resources/Resources.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Resources() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Resources;
---
+++ fitnesstracker/src/components/Weight/Weight.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Weight() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Weight;
