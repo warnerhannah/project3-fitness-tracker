d72a6527386179833e91755bd7954bded73f21a6
Warnerhannah <warnerhannah1@yahoo.com>
2019-04-24 13:18:30 -0700

    starter code, rip my life

--- fitnesstracker/routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const fitnessRoutes = require("./fitnessRoutes");
-
-// Book routes
-router.use("/fitnessRoutes", fitnessRoutes);
-
-module.exports = router;
