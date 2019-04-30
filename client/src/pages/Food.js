bd6831c6433729da47bf7c7a12fc2f96385d1ea5
Warnerhannah <warnerhannah1@yahoo.com>
2019-04-30 13:59:46 -0700

    food component

---
+++ client/src/pages/Food.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import withAuth from '../components/withAuth';
+// import Calendar from "../components/Calendar"
+// import Calories from "../components/Calories"
+// import Weight from "../components/Weight"
+
+
+class Food extends Component {
+    state = {
+
+    }
+
+
+
+    render() {
+        
+    }
+
+
+};
+
+export default withAuth(Food);
