6421f01925ac9b48f9a49bab8bbdd6d75179cef8
michaelolliges <michael1olliges@gmail.com>
2019-04-30 13:59:05 -0700

    switching to michael

--- client/src/components/Calendar.js
+++ client/src/components/Calendar.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import API from "../utils/API";
+import BigCalendar from 'react-big-calendar'
+import events from './events'
+import dates from '../../src/utils/dates'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
 
-class Calendar extends Component {
+const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
 
-    render() {
-        return (
-            <div>
 
-            </div>
-        );
-    };
-}
+let Basic = (props) => (
+  <BigCalendar
+    events={events}
+    views={allViews}
+    step={60}
+    showMultiDayTimes
+    max={dates.add(dates.endOf(new Date(2019, 17, 1), 'day'), -1, 'hours')}
+    defaultDate={new Date(2019, 3, 1)}
+    localizer={localizer}
+  />
+)
 
-export default Calendar;
+export default Basic;
--- client/src/index.js
+++ client/src/index.js
@@ -12,7 +12,8 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
-
+import Calendar from './components/Calendar';
+import Selectable from './components/selectable';
 // Here is if we have an id_token in localStorage
 if(localStorage.getItem("id_token")) {
   // then we will attach it to the headers of each request from react application via axios
@@ -27,6 +28,7 @@ ReactDOM.render(
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/profile" component={Profile} />
+            <Route exact path="/calendar" component={Selectable} />
         </div>
     </Router>
     , document.getElementById('root')
