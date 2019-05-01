5aaa76a1ec473f6dfbfe447ede59a897b4af365f
Warnerhannah <warnerhannah1@yahoo.com>
2019-05-01 12:00:51 -0700

    attempt to edit calendar

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -38,7 +38,9 @@ class Home extends Component {
 
           <div className="calendar">
             <h3>My Workout Schedule</h3>
-            <Calendar />
+            <div>
+              <Calendar />
+            </div>
           </div>
         </div>
 
