fd7eac1c532d39d2af797fe335bb73574022dc4b
Neuromancer93 <itrejonumber1@gmail.com>
2019-05-02 00:34:34 -0700

    connected calories graph to backend

--- client/src/components/Calories.js
+++ client/src/components/Calories.js
@@ -2,10 +2,60 @@ import React, { Component } from "react";
 // import { Bar, Line, Pie } from "react-chartjs-2";
 import { Line } from "react-chartjs-2";
 import withAuth from './../components/withAuth';
-// import API from "../utils/API";
-import Food from "../pages/Food"
+import API from "../utils/API";
+// import Food from "../pages/Food"
 
 class Calories extends Component {
+  state = {
+
+    consumed: [],
+    burned: [],
+    labels: []
+
+  };
+
+  componentDidMount() {
+    API.getCalories().then(res => {
+      const newConsumed = res.data.map(consumed => consumed.consumed);
+      const newBurned = res.data.map(burned => burned.burned);
+      const newLabels = res.data.map(labels => labels.date);
+      this.setState({
+
+        Weight: [],
+        Data: {
+
+          labels: newLabels,
+          datasets: [
+            {
+              label: "Calories Consumed",
+              fill: true,
+              // this changes line fill color
+              backgroundColor: ["rgba(75,192,192,0.4)"],
+              // this changes the color of the dots
+              pointBackgroundColor: "rgb(75,192,192)",
+              borderColor: "rgb(75,192,192)",
+              pointHoverRadius: 5,
+              pointHoverBorderColor: "black",
+              data: newConsumed,
+            },
+            {
+              label: "Calorie Lost",
+              // this changes line fill color
+              backgroundColor: ["rgb(128,1,128,0.6)"],
+              // this changes the color of the dots
+              pointBackgroundColor: "rgb(75,192,192)",
+              borderColor: "rgb(128,1,128)",
+              pointHoverRadius: 5,
+              pointHoverBorderColor: "black",
+              // this displays the users weight on the graph
+              data: newBurned
+            }
+          ]
+        }
+
+      })
+    });
+  };
 
   constructor(props) {
     super(props);
