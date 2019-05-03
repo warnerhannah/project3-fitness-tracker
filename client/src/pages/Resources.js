f15ca2da0b20c1244380c56dfcde45cd37e7e020
Manny <manueltrujillo_02@outlook.com>
2019-05-02 18:28:23 -0700

    pulling from master

--- client/src/pages/Resources.js
+++ client/src/pages/Resources.js
@@ -5,8 +5,12 @@ class Resources extends Component {
 
     render() {
         return (
-            <div>
+            <div className="containerResources">
                 <h3>Fitness Resources:</h3>
+                <div className="content">  
+                    <p><a link="" href="https://www.strongerbyscience.com/" target="_blank">Stronger By Science</a></p>
+                    <p><a link="" href="https://weightology.net" target="_blank">Weightology</a></p>
+                </div>
             </div>
         );
     };
