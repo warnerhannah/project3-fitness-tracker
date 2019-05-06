51ddd81ff06ae1640d1caddf5883a7d47fbf1043
Neuromancer93 <itrejonumber1@gmail.com>
2019-05-06 10:56:39 -0700

    calendar functioning

--- client/src/components/events.js
+++ client/src/components/events.js
@@ -1,18 +1,18 @@
-import API from "../utils/API";
+// import API from "../utils/API";
 
-const now = new Date()
+// const now = new Date()
 
-export default [
-  {
-    id: 14,
-    title: 'Today',
-    start: new Date(new Date().setHours(new Date().getHours() - 3)),
-    end: new Date(new Date().setHours(new Date().getHours() + 3)),
-  },
-  {
-    id: 16,
-    title: 'test7',
-    start: new Date(2019, 4, 3, 1, 0, 0),
-    end: new Date(2019, 4, 3, 2, 0, 0)
-  }
-]
+// export default [
+//   {
+//     id: 14,
+//     title: 'Today',
+//     start: new Date(new Date().setHours(new Date().getHours() - 3)),
+//     end: new Date(new Date().setHours(new Date().getHours() + 3)),
+//   },
+//   {
+//     id: 16,
+//     title: 'test7',
+//     start: new Date(2019, 4, 3, 1, 0, 0),
+//     end: new Date(2019, 4, 3, 2, 0, 0)
+//   }
+// ]
